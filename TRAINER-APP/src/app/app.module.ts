c475a740ec554b21e8ca9abbeff7bc650e048f6f
David Guardia <david.guardia@gmail.com>
2018-10-06 17:50:59 -0400

    adding new module and componentnext is second to pipe

---
+++ TRAINER-APP/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+import { WorkoutRunnerModule } from './workout-runner/workout-runner.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    WorkoutRunnerModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
